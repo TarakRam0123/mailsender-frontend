1d5f60376044d43415124774246e58e2d23c7479
lokeshwarp <panugantilokeshwar2277@gmail.com>
2025-12-23 12:14:02 +0530

    changes to app.js revoked

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import OtpVerify from "./components/OtpVerify";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <OtpVerify />
       <Outlet />
     </>
   );
